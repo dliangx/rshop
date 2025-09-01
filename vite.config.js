fb0b88522690c77a7df3e3b928920d69dc547aad
dengliang <com.liang.deng@outlook.com>
2025-09-01 10:11:15 +0800

    转换成tauri项目

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    strictPort: true,
+  },
+  clearScreen: false,
+  envPrefix: ['VITE_', 'TAURI_'],
+  build: {
+    target: ['es2021', 'chrome100', 'safari13'],
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_DEBUG,
   }
 })
